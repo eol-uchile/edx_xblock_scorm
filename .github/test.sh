115bbe12f10ffb89d7e367b294f8729fe4369346
matsalinas <matsalinas@uchile.cl>
2021-03-08 19:09:38 +0000

    Update Koa tests

--- .github/test.sh
+++ .github/test.sh
@@ -2,14 +2,14 @@
 
 pip install -e /openedx/requirements/edx_xblock_scorm
 
-cd /openedx/requirements/edx_xblock_scorm/scormxblock
+cd /openedx/requirements/edx_xblock_scorm
 cp /openedx/edx-platform/setup.cfg .
 mkdir test_root
 cd test_root/
 ln -s /openedx/staticfiles .
 
-cd /openedx/requirements/edx_xblock_scorm/scormxblock
+cd /openedx/requirements/edx_xblock_scorm
 
-DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest tests.py
+DJANGO_SETTINGS_MODULE=lms.envs.test EDXAPP_TEST_MONGO_HOST=mongodb pytest scormxblock/tests.py
 
 rm -rf test_root
